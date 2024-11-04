d83554466b56f0a674d889ff6f803d8492560b99
Nizar Masadeh <n.masadeh@futeric.com>
2024-11-04 10:07:32 +0300

    almost a finished landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { TextAnim1Component } from './pages/widgets/text-anim1/text-anim1.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'text', component: TextAnim1Component}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AboutComponent } from './pages/about/about.component';
+import { TextAnim1Component } from './pages/widgets/text-anim1/text-anim1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    TextAnim1Component
   ],
   imports: [
     BrowserModule,
